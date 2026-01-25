18f9164f75b722a7d699c0589a87c2eb8ac4a8da
ROOT-360-LK <www.movindu@gmail.com>
2026-01-25 21:16:52 +0530

    fix scroll smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} scroll-smooth antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
